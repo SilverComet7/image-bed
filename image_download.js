66a4995f020a0b8ce704eb90b20ff13441983389
ChrisWang <841531691@qq.com>
2024-11-22 23:20:37 +0800

    feat

--- image_download.js
+++ image_download.js
@@ -3,35 +3,65 @@ const https = require('https'); // 如果图片URL是HTTPS，否则使用http模
 const path = require('path');
 
 // 图片URL和本地保存路径
-const imageUrl = 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/77e0c57214c341bdb22730b58a0ff357~tplv-k3u1fbpfcp-jj-mark:3024:0:0:0:q75.awebp#?w=2672&h=1471&s=603309&e=png&a=1&b=f9f8f8';
-const localFilePath = path.join(__dirname, 'blog', 'image.png');
+const imageUrlList =
+  ['https://img.kancloud.cn/0b/36/0b365eaa878501ec91e0b0be3f6d1238_440x842.png',
+    'https://img.kancloud.cn/fb/3b/fb3b196cc98e89b0869a4c7259da6c76_1080x585.png',
+    'https://img.kancloud.cn/c0/09/c009e16150be055e9e537ec5eb67557a_720x1352.png',
+    'https://img.kancloud.cn/8d/01/8d016e321478fcee1c1fa91765012efa_456x967.png',
+    'https://img.kancloud.cn/0d/57/0d5785a6ada078eb446cf0fb9ce44b1e_440x918.png']
 
 
 
-// 创建文件流
-const fileStream = fs.createWriteStream(localFilePath);
 
-// 使用https模块下载图片
-https.get(imageUrl, (response) => {
-  if (response.statusCode !== 200) {
-    console.error(`Failed to download image, status code: ${response.statusCode}`);
-    return;
-  }
+imageUrlList.forEach(imageUrl => {
+  // 使用https模块下载图片
 
-  response.pipe(fileStream);
+  const localFilePath = path.join(__dirname, 'blog',
+    (new Date().getTime() + '.png')
+  );
+  
+  
+  
+  // 创建文件流
+  const fileStream = fs.createWriteStream(localFilePath);
 
-  fileStream.on('finish', () => {
-    fileStream.close();
-    console.log('Image downloaded successfully.');
-  });
+  
+  https.get(imageUrl, (response) => {
+    if (response.statusCode !== 200) {
+      console.error(`Failed to download image, status code: ${response.statusCode}`);
+      return;
+    }
 
-  response.on('error', (err) => {
-    console.error('Error during download:', err);
-    fs.unlink(localFilePath, () => { }); // 删除部分下载的文件
-  });
-});
+    response.pipe(fileStream);
 
-fileStream.on('error', (err) => {
-  console.error('Error writing to file:', err);
-  fs.unlink(localFilePath, () => { }); // 删除部分下载的文件
-});
+    fileStream.on('finish', () => {
+      fileStream.close();
+    })
+  })
+})
+
+
+// // 使用https模块下载图片
+// https.get(imageUrl, (response) => {
+//   if (response.statusCode !== 200) {
+//     console.error(`Failed to download image, status code: ${response.statusCode}`);
+//     return;
+//   }
+
+//   response.pipe(fileStream);
+
+//   fileStream.on('finish', () => {
+//     fileStream.close();
+//     console.log('Image downloaded successfully.');
+//   });
+
+//   response.on('error', (err) => {
+//     console.error('Error during download:', err);
+//     fs.unlink(localFilePath, () => { }); // 删除部分下载的文件
+//   });
+// });
+
+// fileStream.on('error', (err) => {
+//   console.error('Error writing to file:', err);
+//   fs.unlink(localFilePath, () => { }); // 删除部分下载的文件
+// });
